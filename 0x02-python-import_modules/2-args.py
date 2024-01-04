15d0410095c2f15f677568d2a10dd3c4007376ef
AlaaElrasheed <wethag.234@gmail.com>
2024-01-04 09:12:36 -0800

    jjj

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
         print("{} arguments:".format(len(argv) - 1))
         if len(argv) > 1:
             for i in range(1, len(argv)):
-                print("{}: {}".format(i, argv[i]))                
+                print("{}: {}".format(i, argv[i]))           
